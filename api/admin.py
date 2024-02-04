b756f6ecffff560506c4b898f333d84069aae577
Anurag Nagar <nagaranurag1999@gmail.com>
2024-02-04 10:24:16 +0530

    add: fields and readonly_fields for profile admin class.

--- api/admin.py
+++ api/admin.py
@@ -1,22 +1,29 @@
 from django.contrib import admin
 from api.models import Users, UserProfile
 
+from typing import Tuple
+
 
 @admin.register(Users)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("username", "email", "created_at", "is_active")
-    fields = (
+    list_display: Tuple[str] = ("username", "email", "created_at", "is_active")
+    fields: Tuple[str] = (
         "username",
         "email",
         "password",
+        "new_email",
+        "security_token",
         "created_at",
         "modified_at",
         "last_login",
+        "is_sent",
         "is_active",
         "is_staff",
         "is_superuser",
     )
-    readonly_fields = (
+    readonly_fields: Tuple[str] = (
+        "password",
+        "is_sent",
         "created_at",
         "modified_at",
         "last_login",
@@ -25,4 +32,13 @@ class UserAdmin(admin.ModelAdmin):
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ("user", "bio", "address", "website")
+    list_display: Tuple[str] = ("user", "bio", "address", "website")
+    fields: Tuple[str] = (
+        "user",
+        "bio",
+        "address",
+        "website",
+        "created_at",
+        "modified_at",
+    )
+    readonly_fields: Tuple[str] = ("created_at", "modified_at")
