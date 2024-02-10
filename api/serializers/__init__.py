40778ce07c850ca27fa41b15c95df9be6b05fe01
cjephuneh <calebjephuneh@gmail.com>
2024-02-11 00:15:29 +0300

    updates

--- api/serializers/__init__.py
+++ api/serializers/__init__.py
@@ -1,6 +1,9 @@
 from .login_serializer import UserLoginSerializer
 from .register_serializer import UserRegisterSerializer
 from .profile_serializer import UserDetailSerializer
+from .change_password_serializers import ChangePasswordSerializer
+from .reset_password_serializer import ResetPasswordSerializer, ResetPasswordConfirmSerializer
+from .reset_email_serializer import ResetEmailSerializer
 
 __all__ = [
     "UserLoginSerializer",
